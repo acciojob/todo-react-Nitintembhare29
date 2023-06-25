9bb6c35a4d1e75d485edc6a3dc8acd6ec38fb617
Nitin Tembhare <nitintembhare340@gmail.com>
2023-06-25 14:50:57 +0530

    second commit

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,13 +3,13 @@ import React from "react";
 const TodoList = (props)=>{
      
     return(
-        <div className="list-div">
+        <li className="list-div">
             <ul className="list-item">
                 <div>{props.item}</div>
                 <div
                 onClick={()=>props.deleteitem(props.index)}><button>Delete</button></div>
             </ul>
-        </div>
+        </li>
 
     )
 }
